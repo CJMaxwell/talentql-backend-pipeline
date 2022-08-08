cdae591bcd02c1d9f8c9110fa14c432080b1604c
CJMaxwell <chukwunonyeremmaxwell@gmail.com>
2022-08-08 15:35:31 +0100

    Modified the limit duration

--- src/middleware/apiLimit.ts
+++ src/middleware/apiLimit.ts
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 
 const apiLimit = rateLimit({
-    windowMs: 24 * 60 * 60 * 1000, //1000, // 1 second in milliseconds
+    windowMs: 1000, // 1 second in milliseconds
     max: 3,
     message: 'You have exceeded the 3 requests in 1 second limit!',
     standardHeaders: true,
